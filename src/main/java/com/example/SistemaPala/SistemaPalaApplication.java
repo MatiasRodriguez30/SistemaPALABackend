21beffbf3b261214d60a81ba34515c2903fd14ee
MatiasRodriguez30 <119769021+MatiasRodriguez30@users.noreply.github.com>
2026-05-28 18:29:51 -0300

    Subida del sistema PALA

---
+++ src/main/java/com/example/SistemaPala/SistemaPalaApplication.java
@@ -0,0 +1,13 @@
+package com.example.SistemaPala;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaPalaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaPalaApplication.class, args);
+	}
+
+}
